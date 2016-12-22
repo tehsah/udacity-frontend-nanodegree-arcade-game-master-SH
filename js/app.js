41f616f1d4c799a9c73c142555d01f7681c48c27
tehsah <hanford.scott@gmail.com>
2016-12-21 18:19:02 -0800

    Committing the remaining files

---
+++ js/app.js
@@ -0,0 +1,123 @@
+// Enemies our player must avoid
+var Enemy = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
+    this.col = -1;
+    this.row = randomInt(1, 3);
+    this.x = this.col * 101;
+    this.y = this.row * 83;
+    this.speed = randomInt(1, 10);
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.x = this.x + this.speed;
+    if(this.x > 606) {
+        this.col = -1;
+        this.x = this.col * 101;
+    };
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+var Player = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+    this.col = randomInt(0, 4);
+    this.row = 5;
+    this.x = this.col * 101;
+    this.y = this.row * 83;
+};
+
+Player.prototype.update = function(dt) {
+    var win = false;
+    this.x = this.col * 101;
+    this.y = this.row * 83;
+
+    if(this.row === 0) {
+        win = true;
+    }
+
+    return win;
+};
+
+// new from SH
+Player.prototype.reset = function() {
+    this.row = 5;
+    this.col = randomInt(0, 4);
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(key) {
+    switch(key) {
+        case 'left':
+            this.col--;
+            break;
+        case 'right':
+            this.col++;
+            break;
+        case 'up':
+            this.row--;
+            break;
+        case 'down':
+            this.row++;
+            break;
+    };
+
+    if(this.row < 0) {this.row = 0};
+    if(this.row > 5) {this.row = 5};
+    if(this.col < 0) {this.col = 0};
+    if(this.col > 4) {this.col = 4};
+};
+
+
+// new from SH
+function randomInt(x, y) {
+    return Math.ceil(Math.random() * (y - x)) + x;
+};
+
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+var allEnemies = [];
+for(var i = 0; i < 5; i++) {
+    allEnemies.push(new Enemy());
+};
+
+var player = new Player();
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
